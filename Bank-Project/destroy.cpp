71e747d24b0cf64eb9a1b086f2d8e2118d39727a
Anahit <anah.arakelyan@gmail.com>
2024-04-17 20:48:18 +0400

    tried fixing semaphores

--- Bank-Project/destroy.cpp
+++ Bank-Project/destroy.cpp
@@ -7,67 +7,39 @@
 #include <unistd.h>
 #include <cstring>
 #include <cerrno>
+#include <sstream>
+#include <semaphore.h>
 
 #include "bank.h"
 
 int main()
 {
-    const char* sem_name = "/sem_shared_mem";
-    int sem_shm = shm_open(sem_name, O_CREAT | O_RDWR | O_TRUNC, 0666);
-    if(sem_shm == -1)
-    {
-        std::cerr << "sem shm open" << std::endl;
-        exit(errno);
-    }
 
-    const char* client_name = "/client_id_shm";
-    int client_shm = shm_open(client_name, O_CREAT | O_RDWR | O_TRUNC, 0666);
-    if(client_shm == -1)
-    {
-        std::cerr << "sem shm open" << std::endl;
-        exit(errno);
-    }
     const char* shm_name = "/bank_shared_mem";
-    int shm_fd = shm_open(shm_name, O_RDWR, 0666);
-    if(shm_fd == -1)
+    const char* sem_name = "/sem_shared_mem";
+//    int shm_fd = shm_open(shm_name, O_RDWR, 0666);
+//    if(shm_fd == -1)
+//    {
+//        std::cerr << "sem shm open" << std::endl;
+//        exit(errno);
+//    }
+    if(sem_unlink(sem_name) == -1)
     {
-        std::cerr << "sem shm open" << std::endl;
-        exit(errno);
+        std::cerr << "sem_unlink" << std::endl;
+        exit(EXIT_FAILURE);
     }
 
-    if(shm_unlink(sem_name) == -1)
-    {
-        std::cerr << "shm_unlink" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-    if(shm_unlink(client_name) == -1)
-    {
-        std::cerr << "shm_unlink" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-    if(shm_unlink(shm_name) == -1)
-    {
+    if(shm_unlink(shm_name) == -1) {
         std::cerr << "shm_unlink" << std::endl;
         exit(EXIT_FAILURE);
     }
 
-    if(close(sem_shm) == -1)
-    {
-        std::cerr << "init close" <<std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    if(close(client_shm) == -1)
-    {
-        std::cerr << "init close" <<std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    if(close(shm_fd) == -1)
-    {
-        std::cerr << "init close" <<std::endl;
-        exit(EXIT_FAILURE);
-    }
+//
+//    if(close(shm_fd) == -1)
+//    {
+//        std::cerr << "init close" <<std::endl;
+//        exit(EXIT_FAILURE);
+//    }
 
 
     return 0;
--- Bank-Project/init.cpp
+++ Bank-Project/init.cpp
@@ -9,45 +9,14 @@
 
 #include "bank.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    const int n = 10;
-    const char* sem_name = "/sem_shared_mem";
-    int sem_shm = shm_open(sem_name, O_CREAT | O_RDWR | O_TRUNC, 0666);
-    if(sem_shm == -1)
+    if(argc != 2)
     {
-        std::cerr <<"sem shm open"<<std::endl;
-        exit(errno);
+        std::cout<<"Can't create bank"<<std::endl;
+        exit(EXIT_FAILURE);
     }
-    if(ftruncate(sem_shm, n * sizeof(sem_t)) == -1)
-    {
-        std::cerr << "sem ftruncate" << std::endl;
-        exit(errno);
-    }
-    sem_t* sems = (sem_t*)mmap(nullptr, n*sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, sem_shm, 0);
-//    for(int i = 0; i < n; ++i)
-//    {
-//        if(sem_init(&sems[i], 1, 1) == -1)
-//        {
-//            std::cerr << "sem_init" <<std::endl;
-//            exit(errno);
-//        }
-//    }
-
-    const char* client_name = "/client_id_shm";
-    int client_shm = shm_open(client_name, O_CREAT | O_RDWR | O_TRUNC, 0666);
-    if(client_shm == -1)
-    {
-        std::cerr <<"sem shm open"<<std::endl;
-        exit(errno);
-    }
-    if(ftruncate(client_shm, sizeof(int)) == -1)
-    {
-        std::cerr << "sem ftruncate" << std::endl;
-        exit(errno);
-    }
-    int* id = (int*)mmap(nullptr, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, client_shm, 0);
-
+    const int n = std::stoi(argv[1]);
 
     const char* shm_name = "/bank_shared_mem";
     int shm_fd = shm_open(shm_name, O_CREAT | O_RDWR , 0666);
@@ -56,7 +25,7 @@ int main()
         std::cerr << "shm_open" <<std::endl;
         exit(errno);
     }
-    //shm open semaphore max 10 hat
+
     std::size_t size = sizeof(Bank) + n * sizeof(BankCell);
 
     if(ftruncate(shm_fd, size) == -1)
@@ -86,30 +55,9 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    if(munmap(sems, n * sizeof(sem_t)) == -1)
-    {
-        std::cerr << "sem init munmap" <<std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    if(munmap(id, sizeof(int)) == -1)
-    {
-        std::cerr << "id init munmap" <<std::endl;
-        exit(EXIT_FAILURE);
-    }
     if(close(shm_fd) == -1)
     {
         std::cerr << "init close" <<std::endl;
-        exit(EXIT_FAILURE);
-    }
-    if(close(sem_shm) == -1)
-    {
-        std::cerr << "init close" <<std::endl;
-        exit(EXIT_FAILURE);
-    }
-    if(close(client_shm) == -1)
-    {
-        std::cerr << "close" <<std::endl;
         exit(EXIT_FAILURE);
     }
 
